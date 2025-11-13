d3ef3b58120870ac18075b125eb01281d468a109
Fletcher Liverance <fliver@amazon.com>
2025-11-13 11:03:47 -0800

    Add scripts and package dir to setup.py to properly include scripts during training.

--- parkour/setup.py
+++ parkour/setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name="Isaaclab_Parkour",
     version="0.1",    
-    packages=find_packages(),  
+    packages=find_packages(include=['scripts', 'scripts.*']),
+    package_dir={'': '.'},
 )
